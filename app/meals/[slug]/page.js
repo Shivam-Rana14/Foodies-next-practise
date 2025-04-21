914dd67f698fd6c2e7f6c4a0973536c04f950a12
Shivam Rana <shivamrana.cpp@gmail.com>
2025-04-21 18:45:56 +0530

    Initial commit

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MealDetailsPage({ params }) {
+  return (
+    <main>
+      <h1>Meal Details</h1>
+      <p>{params.slug}</p>
+    </main>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ShareMealPage() {
+  return (
+    <main>
+      <h1>Share Meals</h1>
+    </main>
+  );
+}
